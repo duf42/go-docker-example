cba8d00ee19e806696b58f84c0b6d5a4c9ac26d6
Duf <odufour.dev@gmail.com>
2022-08-07 23:44:05 +0200

    debug: Try to only display a message

--- sources/main.go
+++ sources/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
     "fmt"
-    "html"
-    "log"
-    "net/http"
+    //"html"
+    //"log"
+    //"net/http"
     "os"
 )
 
 func main() {
 
+   fmt.Println("Current port is " + os.Getenv("PORT"));
+/*
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
@@ -19,5 +21,5 @@ func main() {
     })
 
     log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), nil))
-
+*/
 }

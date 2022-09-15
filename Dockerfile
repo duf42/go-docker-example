81c5b8697743f2770184469ed54a23afe35d3674
Duf <odufour.dev@gmail.com>
2022-09-15 22:22:53 +0200

    tests: Pass with C library

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM golang:1.14.3-alpine AS base
 
 # Install GCC
-RUN apk add build-base
+RUN apk add build-base libc6-compat
 RUN mkdir /deps
 
 WORKDIR /src
 
-ENV CGO_ENABLED=0
+ENV CGO_ENABLED=1
 ENV GOPATH=/
+ENV GOOS=linux 
+ENV GOARCH=amd64
+ENV CC=gcc
 
 COPY ./lib /deps
 COPY ./sources .
@@ -15,17 +18,17 @@ COPY ./tests .
 
 FROM base AS build
 
-RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=gcc go build -o /out/server .
+RUN go build -o /out/server .
 
 FROM base AS unit-test
+ENV LD_LIBRARY_PATH=/deps
 ENTRYPOINT ["go", "test"]
 CMD ["-v", "./..."]
 
 FROM alpine:latest
 
-RUN mkdir /config
-RUN mkdir /install
-RUN mkdir /web
+RUN mkdir /config && mkdir /deps && mkdir /install && mkdir /web
+ENV LD_LIBRARY_PATH=/deps
 
 COPY --from=build /out/server /install/server
 COPY ./web /web
--- sources/model/model.go
+++ sources/model/model.go
@@ -17,7 +17,7 @@ type (
 	parameters_T C.struct_Parameters_T
 )
 */
-/*
+
 type inputs_T struct {
     target float64
     current float64
@@ -38,23 +38,35 @@ type simdata_T struct {
 }
 
 var simdata simdata_T
-*/
 
 func Initialize() error {
 
-    C.Model_U.target  = C.double(0.1)
-    C.Model_U.current = C.double(0.2)
+	simdata.inputs.target = 0.1
+	simdata.inputs.current = 0.2
+	simdata.outputs.command = 0.3
+	simdata.parameters.Kp = 0.4
 
-    C.Model_Y.command = C.double(0.3)
+    C.Model_U.target  = C.double(simdata.inputs.target)
+    C.Model_U.current = C.double(simdata.inputs.current)
 
-    C.Model_P.Kp = C.double(0.4)
+    C.Model_Y.command = C.double(simdata.outputs.command)
+
+    C.Model_P.Kp = C.double(simdata.parameters.Kp)
 
     return nil
 
 }
 
 func Step() error {
+
+	C.Model_U.target = C.double(simdata.inputs.target)
+	C.Model_U.current = C.double(simdata.inputs.current)
+	C.Model_P.Kp = C.double(simdata.parameters.Kp)
+
     C.Model_Y.command = (C.Model_U.target - C.Model_U.current) * C.Model_P.Kp
+
+	simdata.outputs.command = float64(C.Model_Y.command)
+
     return nil
 }
 
@@ -69,19 +81,23 @@ func GetOutput(name string) float64 {
 }
 
 func GetParameter(name string) float64 {
-    if name == "Kp"{
+    
+	if name == "Kp"{
         return float64(C.Model_P.Kp)
     } else {
         return 0.0
     }
+
 }
 
 func SetInput(name string, value float64) error {
 
     if name == "target" {
-        C.Model_U.target = C.double(value)
+        simdata.inputs.target = value
+		C.Model_U.target = C.double(simdata.inputs.target)		
     } else if name == "current" {
-        C.Model_U.current = C.double(value)
+		simdata.inputs.current = value
+        C.Model_U.current = C.double(simdata.inputs.current)
     }
     return nil
 
@@ -90,7 +106,8 @@ func SetInput(name string, value float64) error {
 func SetParameter(name string, value float64) error {
 
     if name == "Kp" {
-        C.Model_P.Kp = C.double(value)
+		simdata.parameters.Kp = value
+        C.Model_P.Kp = C.double(simdata.parameters.Kp)
     }
     return nil
 
